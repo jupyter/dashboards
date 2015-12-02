b885e02922ca2f5bc207d5dac4c3ac8f8a63a677
Peter Parente <pparent@us.ibm.com>
2015-12-02 15:48:11 -0500

    Improve package metadata
    (c) Copyright IBM Corp. 2015

--- setup.py
+++ setup.py
@@ -50,11 +50,22 @@ setup(
     name='jupyter_dashboards',
     author='Jupyter Development Team',
     author_email='jupyter@googlegroups.com',
-    description='IPython / Jupyter extensions to enable dashboard creation and deployment',
+    description='Extension for Jupyter Notebook 4.0.x for laying out, viewing, and deploying notebooks as dynamic dashboards',
+    long_description = '''
+    This package adds the following features to Jupyter Notebook:
+
+* Dashboard layout mode for arranging notebook cell outputs in a grid-like fashion
+* Dashboard view mode for interacting with an assembled dashboard within the Jupyter Notebook
+* Ability to share notebooks with dashboard layout metadata in them with other Jupyter Notebook users
+* Ability to nbconvert a notebook to a separate dashboard web application
+
+See `the project README <https://github.com/jupyter-incubator/dashboards>`_
+for more information. 
+''',
     url='https://github.com/jupyter-incubator/dashboards',
     version=VERSION_NS['__version__'],
     license='BSD',
-    platforms=['IPython Notebook 4.x'],
+    platforms=['Jupyter Notebook 4.0.x'],
     packages=[
         'urth', 
         'urth.dashboard', 
@@ -65,5 +76,16 @@ setup(
     install_requires=[],
     cmdclass={
         'install': InstallCommand
-    }
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+    ]
 )
