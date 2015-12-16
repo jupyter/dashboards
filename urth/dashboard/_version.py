756725516afe75e6123c9f9ed5d263f67e9ec82d
Peter Parente <pparent@us.ibm.com>
2015-12-15 22:09:18 -0500

    Bump to 0.2.3.dev

--- urth/dashboard/_version.py
+++ urth/dashboard/_version.py
@@ -1,5 +1,5 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 2, 2)
+version_info = (0, 2, 3, 'dev')
 __version__ = '.'.join(map(str, version_info))
