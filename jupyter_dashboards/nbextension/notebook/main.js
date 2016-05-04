3a1be7ca7f32bf5e506cdfdef97faaeef7bdc7f0
Nitin Dahyabhai <nitind@us.ibm.com>
2016-05-04 17:00:35 -0400

    [Issue: 202] Multiple pasted cells get the same layout     Replace dashboard layout metadata in copied cells with "hidden."     This unfortunately does affect the Cut operation as well. (c) Copyright IBM Corp. 2016

--- jupyter_dashboards/nbextension/notebook/main.js
+++ jupyter_dashboards/nbextension/notebook/main.js
@@ -5,11 +5,29 @@
 
 /* global define */
 define([
-    './dashboard-view/dashboard-view'
-], function() {
+    './dashboard-view/dashboard-view',
+    'notebook/js/notebook'
+], function(
+    View,
+    Notebook
+) {
     return {
         load_ipython_extension: function() { 
-            console.debug('jupyter_dashboards loaded'); 
+            console.debug('jupyter_dashboards loaded');
+            if (Notebook.Notebook.prototype.copy_cell) {
+                Notebook.Notebook.prototype.__copy_cell = Notebook.Notebook.prototype.copy_cell;
+                Notebook.Notebook.prototype.copy_cell = function() {
+                    this.__copy_cell();
+                    for (var i = 0; i < this.clipboard.length; i++) {
+                        var cell_json = this.clipboard[i];
+                        if (cell_json.metadata.urth !== undefined && cell_json.metadata.urth.dashboard !== undefined && cell_json.metadata.urth.dashboard.layout !== undefined) {
+                            // other dashboard metadata may need to be preserved
+                            delete cell_json.metadata.urth.dashboard.layout;
+                            cell_json.metadata.urth.dashboard.hidden = true;
+                        }
+                    }
+                }
+            }
         }
     };
 });
